89c190badfeb7e7e8d5484bc6c78d512bb13a92b
Chadwick Butts <ccbutts@mail.usf.edu>
2022-05-31 00:37:25 -0400

    fixed async await bug

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import axios from 'axios';
 
 export default function Search(props) {
 
     let onClick = (category) => async (event) => {
-        let response = await fetch(`https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972&categories=${category}`);
-        let data = await response.json();
+       let api_key = 'dxirLfvnmFlvxIpvmBrotrasncTduFC-Uc-pG3wsjVOz2uvdPc7X2PFQIvWnNc3VSZgqCmyFLlC4YdN94LITe_-DtvL7yyiaYKho_10Ug3V0mxycgiSErmnCmEuAYnYx'
+        // axios.interceptors.request.use(
+        //     config => {
+        //         config.headers.authorization = `Bearer ${api_key}`;
+        //         config.headers.mode = 'no-cors';
+        //         return config;
+        //     },
+        //     error => {
+        //         return Promise.reject(error);
+        //     }
+        // )
+
+        axios.defaults.headers.common['Authorization'] = `Bearer ${api_key}`;
+        let data = await axios.get(`https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972&categories=${category}`);
         console.log(data);
     }
-
     return (
         <div>
             <button id="vegan" onClick={onClick("vegan")}>Vegan</button>
@@ -15,7 +27,3 @@ export default function Search(props) {
         </div>
     )
 }
-
-
-
-
